7b0c449fb3b3d3db856215493f985f30bb960b95
Shawn Yen <yrs791123@hotmail.com>
2023-07-17 22:20:40 +0800

    改成hash路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [{ path: 'board', component: BoardComponent },
                         { path: '', component: IndexComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
